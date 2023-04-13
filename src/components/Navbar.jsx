d978e3638f26a046f2b36fc81d237e0989a96d1d
HussienSayed14 <hussiens399@gmail.com>
2023-04-13 22:05:13 +0200

    Navbar component with login and logout conditions

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Logo from '../assets/Logo2.png'
+import '../styles/Navbar.css'
+
+let loggedIn = false;
+
+
+function alo(){
+    return <li><a href='#'>Logout</a></li>;
+}
+
+function isLogged(){
+    if(loggedIn){
+        return <li><a href='#'>Logout</a></li>;
+        }
+    else{
+        
+           return( [
+             <li><a href='#'>Sign Up</a></li> ,
+             <li><a href='#'>Login</a></li> 
+           ]
+           )
+            
+        
+        
+
+    }
+
+    //return null;
+}
+
+
+function Navbar() {
+    
+  return (
+    <div className='Navbar'>
+        <nav>
+            <img className='Nav-img' src={Logo}></img>
+            <div>
+                <ul id='nav-items'>
+                    <li><a className='active' href='#'>Home</a></li>
+                    <li><a href='#'>Schools</a></li>
+                    <li><a href='#'>Our Services</a></li>
+                    <li><a href='#'>Contact Us</a></li>
+                    {isLogged()}
+                    
+                    </ul>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
