6f5826d086aa5ab9ce820f4409b5daa1a5eb441b
HussienSayed14 <hussiens399@gmail.com>
2023-06-11 16:51:19 +0300

    Sign Up and Login added

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,23 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 import HomePage from './components/HomePage'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div className='Navbar'>
-        <HomePage/>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" exact Component={HomePage} />
+          <Route path="/login" exact Component={Login} />
+          <Route path="/signup" exact Component={SignUp} />
+       </Routes>
+
+      </Router>
       
     </div>
   )
