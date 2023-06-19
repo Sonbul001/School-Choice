f86ced16f9c263e9af6d1827e9de182baa017cf7
Zoldikxx <mziad316@yahoo.com>
2023-06-19 21:48:52 +0300

    Solved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import HomePage from './components/HomePage/HomePage'
 import Login from './components/LoginPage/Login'
 import SignUp from './components/SignUpPage/SignUp'
 import DetailedSearchPage from './components/DetailedSearch/DetailedSearch'
 import ExamsPage from './components/Exams/ExamsPage'
+import AppliedSchools from './components/SavedSchools/SavedSchools'
 import './App.css'
 import CoursesPage from './components/Courses/CoursesPage'
 
 function App() {
-  
+
 
   return (
     <div className="App">
@@ -20,11 +21,10 @@ function App() {
           <Route path="/search" exact Component={DetailedSearchPage} />
           <Route path="/exams" exact Component={ExamsPage} />
           <Route path="/courses" exact Component={CoursesPage} />
-
-          
+          <Route path="/appliedSchools" exact Component={AppliedSchools} />
         </Routes>
       </Router>
-      
+
     </div>
   )
 }
---
+++ src/components/SavedSchools/SavedSchools.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import Navbar from '../NavBar/Navbar'
+import Footer from '../Footer/Footer';
+import Sidebar from '../SideBar/Sidebar';
+import Cards from '../Cards/Cards';
+import Card from '../Cardss/Card/Card';
+import Carousel from '../Tests/Carousel/Carousel';
+import './SavedSchools.css';
+
+
+function SavedSchools() {
+    const recommendedSchoolsData = [
+        { title: 'Card 1', content: 'Card content 1', item: 'card item 1', item2: 'card item2 1' },
+        { title: 'Card 2', content: 'Card content 2', item: 'card item 2', item2: 'card item2 2' },
+        { title: 'Card 3', content: 'Card content 3', item: 'card item 3', item2: 'card item2 3' },
+        { title: 'Card 1', content: 'Card content 1', item: 'card item 1', item2: 'card item2 1' },
+        { title: 'Card 2', content: 'Card content 2', item: 'card item 2', item2: 'card item2 2' },
+        { title: 'Card 3', content: 'Card content 3', item: 'card item 3', item2: 'card item2 3' },
+        { title: 'Card 1', content: 'Card content 1', item: 'card item 1', item2: 'card item2 1' },
+        { title: 'Card 2', content: 'Card content 2', item: 'card item 2', item2: 'card item2 2' },
+        { title: 'Card 3', content: 'Card content 3', item: 'card item 3', item2: 'card item2 3' }
+    ];
+    return (
+        <div className='appliedSchools--component'>
+            <div className='navBar'>
+                <Navbar />
+            </div>
+
+            <div className="appliedSchools--component--content">
+                <Sidebar />
+                <div className="appliedSchools--component--main">
+                    <h2>Saved Schools</h2>
+                    <div className='cards'>
+                        <Cards />
+                    </div>
+                </div>
+            </div>
+
+            <div className="savedSchools--component--recommended">
+                <h2>Recommended Schools</h2>
+                <div style={{ maxWidth: 1150, marginLeft: "auto", marginRight: "auto", marginTop: 5 }}>
+                    <Carousel show={3}>
+                        {recommendedSchoolsData.map((recommendedSchool, index) => (
+                            <div className='cards' key={index}>
+                                <Card
+                                    key={index}
+                                    cardTitle={recommendedSchool.title}
+                                    cardContent={recommendedSchool.content}
+                                    cardItem={recommendedSchool.item}
+                                    cardItem2={recommendedSchool.item2}
+                                />
+                            </div>
+                        ))}
+                    </Carousel>
+                </div>
+            </div>
+
+            <div className='footer'>
+                <Footer />
+            </div>
+
+        </div>
+    )
+}
+
+
+export default SavedSchools
