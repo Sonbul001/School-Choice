168e8674f812d62e23c17dbb2bfc8a0513060d61
Zoldikxx <mziad316@yahoo.com>
2023-06-22 16:14:05 +0300

    Modified some CSS

--- src/components/SavedCourses/SavedCourse/SavedCourse.jsx
+++ src/components/SavedCourses/SavedCourse/SavedCourse.jsx
@@ -6,16 +6,22 @@ import './SavedCourse.css'
 
 export default function SavedCourseInfo(props) {
     return (
-        <div className='courseInfo--component'>
-            <Course
-                logo='../../../assets/Logo2.png'
-                courseName={props.courseName}
-                schoolType={props.schoolType}
-                coursePriceType={props.coursePriceType}
-                courseLocation={props.courseLocation}
-                courseProvider={props.courseProvider}
-            />
+        <div className='course--component--container'>
+
+            <div className='course--component--left'>
+                <Course
+                    logo='../../../assets/Logo2.png'
+                    courseName={props.courseName}
+                    schoolType={props.schoolType}
+                    coursePriceType={props.coursePriceType}
+                    courseLocation={props.courseLocation}
+                    courseProvider={props.courseProvider}
+                />
+            </div>
+
             <CourseInfo description={props.description} />
+
         </div>
+
     )
 }
