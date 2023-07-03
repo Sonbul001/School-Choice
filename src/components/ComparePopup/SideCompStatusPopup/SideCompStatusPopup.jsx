513c76e5b5195c5ccfc31c0d05bfc744667a5a4d
Hatem64 <ah.hatem225@gmail.com>
2023-07-03 14:48:02 +0300

    fixed the compare button showing up on less than 2 schools

--- src/components/ComparePopup/SideCompStatusPopup/SideCompStatusPopup.jsx
+++ src/components/ComparePopup/SideCompStatusPopup/SideCompStatusPopup.jsx
@@ -25,6 +25,8 @@ export default function SideCompStatusPopup(props) {
 	useEffect(() => {
 		if (props.schools.length === 2) {
 			setClickable(true);
+		} else {
+			setClickable(false);
 		}
 	}, [props.schools]);
 
