1b6d64f6c810bf2ecf1c7a2a254c4c8e59950370
Karim Sonbul <sonbul.karim@gmail.com>
2023-07-05 03:06:21 +0200

    saved token in localstorage and did some applicant functions

--- src/components/SchoolCardComp/SchoolCard/SchoolCard.jsx
+++ src/components/SchoolCardComp/SchoolCard/SchoolCard.jsx
@@ -9,12 +9,44 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import "./SchoolCard.css";
 
 function SchoolCard(props) {
-	const { name, address, type, gender, rating, startingFees, map } = props.schoolInfo;
+	const { name, logo, about, type, educationLevel, gender, address, advertised, map, city, images, website, phones, email, MinimumFee, MaximumFee } = props.schoolInfo;
 	library.add(faFlag, faPeopleGroup, faStar);
 	// const [rate, setRate] = useState(0);
 	// const handleRating = (value) => {
 	// 	setRate(value);
 	// };
+	// {
+    //     "id": 1,
+    //     "name": "Misr Language Schools 232",
+    //     "logo": "logo",
+    //     "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam nisl lacinia nisl, vitae aliqua",
+    //     "type": [
+    //         "American",
+    //         "Lycee",
+    //         "German",
+    //         "Canadian"
+    //     ],
+    //     "educationLevel": [
+    //         "Preparatory",
+    //         "Secondary"
+    //     ],
+    //     "gender": "Mixed",
+    //     "address": "Address",
+    //     "advertised": false,
+    //     "map": "google.maps/example",
+    //     "city": "zayed",
+    //     "images": null,
+    //     "website": "http://www.example.com",
+    //     "phones": [
+    //         "0123456789",
+    //         "0123456789"
+    //     ],
+    //     "email": "school@gmail.com",
+    //     "MinimumFee": 1000,
+    //     "MaximumFee": 5000,
+    //     "schoolReview": [],
+    //     "feesSection": []
+    // }
 	return (
 		<div id="cardd">
 			<Card className="school-card">
@@ -26,17 +58,13 @@ function SchoolCard(props) {
 								<FontAwesomeIcon className="school-card-header-left-icons-item" icon="fa-solid fa-people-group" />
 							</Col>
 							<Col>
-								<Card.Text className="school-card-header-left-text">{type}</Card.Text>
+								<Card.Text className="school-card-header-left-text">{Array.isArray(type) ? type.join(",") : ""}</Card.Text>
 								<Card.Text className="school-card-header-left-text">{gender}</Card.Text>
 							</Col>
 						</div>
-						<Col className="school-card-header-right">
-							<Card.Text className="school-card-header-right-text">{rating}</Card.Text>
-							<FontAwesomeIcon className="school-card-header-right-icons" icon="fa-solid fa-star" />
-						</Col>
 					</Row>
 				</Card.Header>
-				<Card.Img className="school-card-img" variant="top" src={Logo} />
+				<Card.Img className="school-card-img" variant="top" src={logo} />
 				<Card.Body className="school-card-body">
 					<Card.Title className="school-card-body-title">{name}</Card.Title>
 					<small className="school-card-body-small">{address}</small>
@@ -44,7 +72,7 @@ function SchoolCard(props) {
 				<Card.Footer className="school-card-footer">
 					<Row className="school-card-footer-row">
 						<Col className="school-card-footer-row-left col-md-8">
-							<small className="school-card-footer-row-left-text">Starting Fees: {startingFees} EGP</small>
+							<small className="school-card-footer-row-left-text">Starting Fees: {MinimumFee} EGP</small>
 						</Col>
 						<Col className="school-card-footer-row-right col-md-4">
 							<Button className="school-card-footer-row-right-button" href={map}>
--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Sidebar.css'
 
 import usernameLogo from '../../assets/username-logo.png';
@@ -6,29 +6,49 @@ import favoritesLogo from '../../assets/favorites-logo.png';
 import testsLogo from '../../assets/tests-logo.png';
 import coursesLogo from '../../assets/courses-logo.png';
 import settingsLogo from '../../assets/settings-logo.png';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
+    const [username, setUserName] = useState("");
+    useEffect(() => {
+        const getApplicant = async () => {
+          try {
+            const response = await fetch('http://localhost:3000/applicants/profile', {
+              headers: {
+                Authorization: 'Bearer ' + localStorage.getItem('token')
+              }
+            });
+            if (response.status != 401) {
+              const data = await response.json();
+              setUserName(data.fullName);
+            } else {
+              throw new Error('Unauthorized');
+            }
+          } catch (error) {
+            console.error('Error:', error);
+            alert('Failed to fetch data from the server.');
+          }
+        };
+    
+        getApplicant();
+      }, []);
     return (
         <div className="sidebar--component">
             <div className="sidebar--component--item">
                 <img src={usernameLogo} alt="Username Logo" />
-                <a href="#">Username</a>
+                <Link to={"/applicant"}>{username}</Link>
             </div>
             <div className="sidebar--component--item">
                 <img src={favoritesLogo} alt="Favorites Logo" />
-                <a href="#">Favorites</a>
+                <Link to={"/savedSchools"}>Schools</Link>
             </div>
             <div className="sidebar--component--item">
                 <img src={testsLogo} alt="Tests Logo" />
-                <a href="#">Exams</a>
+                <Link to={"/savedTests"}>Exams</Link>
             </div>
             <div className="sidebar--component--item">
                 <img src={coursesLogo} alt="Courses Logo" />
-                <a href="#">Courses</a>
-            </div>
-            <div className="sidebar--component--item">
-                <img src={settingsLogo} alt="Settings Logo" />
-                <a href="#">Settings</a>
+                <Link to={"/savedCourses"}>Courses</Link>
             </div>
         </div>
     );
