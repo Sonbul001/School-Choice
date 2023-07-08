881b7215e729febf326517831730153cdcd9d633
Hatem64 <ah.hatem225@gmail.com>
2023-07-09 00:40:12 +0300

    input fixed

--- src/components/AdminsDashboardTests/AddTestPopup/AddTestPopup.jsx
+++ src/components/AdminsDashboardTests/AddTestPopup/AddTestPopup.jsx
@@ -37,15 +37,15 @@ function AddTestPopup(props) {
 	const closePopup = () => {
 		props.openClosePopup();
 	};
-	// name varchar(255) 
-	// subject varchar(255) 
-	// description varchar(255) 
-	// maxGrade int 
-	// duration int 
-	// link varchar(255) 
-	// classroom varchar(255) 
-	// price int 
-	// logo varchar(255) 
+	// name varchar(255)
+	// subject varchar(255)
+	// description varchar(255)
+	// maxGrade int
+	// duration int
+	// link varchar(255)
+	// classroom varchar(255)
+	// price int
+	// logo varchar(255)
 	// type varchar(255)
 
 	return (
@@ -58,23 +58,23 @@ function AddTestPopup(props) {
 					<Form onSubmit={handleSubmit}>
 						<Form.Group className="add-test-popup-test-name">
 							<Form.Label className="add-test-popup-test-name-label">Test Name</Form.Label>
-							<Form.Control className="add-test-popup-test-name-control" type="text" placeholder="Enter test Name" name="testName" required value={test.name} onChange={handleChange} />
+							<Form.Control className="add-test-popup-test-name-control" type="text" placeholder="Enter test Name" name="name" required value={test.name} onChange={handleChange} />
 						</Form.Group>
 						<Form.Group className="add-test-popup-test-about">
 							<Form.Label className="add-test-popup-test-about-label">Description</Form.Label>
-							<Form.Control className="add-test-popup-test-about-control" as="textarea" rows={3} placeholder="Enter test About" name="about" required value={test.description} onChange={handleChange} />
+							<Form.Control className="add-test-popup-test-about-control" as="textarea" rows={3} placeholder="Enter test About" name="description" required value={test.description} onChange={handleChange} />
 						</Form.Group>
 						<Form.Group className="add-test-popup-test-type">
 							<Form.Label className="add-test-popup-test-type-label">Test Subject</Form.Label>
-							<Form.Control className="add-test-popup-test-type-control" type="text" placeholder="Enter test Type" name="schoolType" required value={test.subject} onChange={handleChange} />
+							<Form.Control className="add-test-popup-test-type-control" type="text" placeholder="Enter test Type" name="subject" required value={test.subject} onChange={handleChange} />
 						</Form.Group>
 						<Form.Group className="add-test-popup-test-link">
 							<Form.Label className="add-test-popup-test-link-label">Test Link</Form.Label>
-							<Form.Control className="add-test-popup-test-link-control" type="url" placeholder="Enter test Link" name="testLink" required value={test.link} onChange={handleChange} />
+							<Form.Control className="add-test-popup-test-link-control" type="url" placeholder="Enter test Link" name="link" required value={test.link} onChange={handleChange} />
 						</Form.Group>
 						<Form.Group className="add-test-popup-test-price">
 							<Form.Label className="add-test-popup-test-price-label">Test Price</Form.Label>
-							<Form.Control className="add-test-popup-test-price-control" type="number" min="0" placeholder="Enter test Price" name="testPrice" required value={test.price} onChange={handleChange} />
+							<Form.Control className="add-test-popup-test-price-control" type="number" min="0" placeholder="Enter test Price" name="price" required value={test.price} onChange={handleChange} />
 						</Form.Group>
 						<div className="add-test-popup-button-container">
 							<Button className="add-test-popup-button" type="submit">
